db00573568001f95da55bb6e8042b66d3e977463
norcodes987 <norcodes987@gmail.com>
2024-01-16 23:54:44 +0800

    overview

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,7 +46,7 @@ import {
       icon: web,
     },
     {
-      title: "React Native Developer",
+      title: "Mobile Developer",
       icon: mobile,
     },
     {
@@ -54,7 +54,7 @@ import {
       icon: backend,
     },
     {
-      title: "Content Creator",
+      title: "DevOps Engineer",
       icon: creator,
     },
   ];
