07d4d74cece55e3d56bc6114add5aeaf97341819
norcodes987 <norcodes987@gmail.com>
2024-01-17 18:20:32 +0800

    experience and technologies

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useLayoutEffect, useRef } from "react";
+import {motion} from 'framer-motion'
+import {textVariant} from '../utils/motion'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import {techItems} from '../constants'
+import {styles} from '../styles'
 
 const Tech = () => {
+  const component = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
+
+ 
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      // create as many GSAP animations and/or ScrollTriggers here as you want...
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          pin: true, // pin the trigger element while active
+          start: "top bottom",
+          end: "bottom top",
+          scrub: 4,
+        },
+      });
+
+      tl.fromTo(
+        ".tech-row",
+        {
+          x: (index) => {
+            return index % 2 === 0
+              ? gsap.utils.random(600, 400)
+              : gsap.utils.random(-600, -400);
+          },
+        },
+        {
+          x: (index) => {
+            return index % 2 === 0
+              ? gsap.utils.random(-600, -400)
+              : gsap.utils.random(600, 400);
+          },
+          ease: "power1.inOut",
+        },
+      );
+    }, component);
+    return () => ctx.revert(); // cleanup!
+  }, []);
+
+
   return (
-    <div>Tech</div>
+    <section className="overflow-hidden" ref={component}>
+     
+        <motion.div variants={textVariant()} className={`${styles.padding} max-w-7xl mx-auto relative z-0`}>
+        <p className={styles.sectionSubText}>What I use</p>
+        <h2 className={styles.sectionHeadText}>Technologies.</h2>
+      </motion.div>
+   
+     
+      <div>
+        {techItems.map((tech, index) => (
+          <div
+            key={index}
+            className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
+            aria-label={tech.tech_name || ""}
+          >
+            {Array.from({ length: 15 }, (_, index) => (
+              <React.Fragment key={index}>
+                <span
+                  className={"tech-item text-8xl font-extrabold uppercase tracking-tighter"}
+                  style={{
+                    color: index === 7 && tech.tech_color ? tech.tech_color : "inherit",
+                  }}
+                >
+                  {tech.tech_name}
+                </span>
+              </React.Fragment>
+            ))}
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
-export default Tech
+export default Tech;
--- src/constants/index.js
+++ src/constants/index.js
@@ -15,10 +15,10 @@ import {
     git,
     figma,
     docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
+    ibm,
+    mci,
+    hpe,
+    nex10,
     carrent,
     jobit,
     tripguide,
@@ -116,58 +116,62 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
-      iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      title: "Assistant Manager (HR Analytics and Performance Management)",
+      company_name: "Ministry of Communications and Information",
+      icon: mci,
+      iconBg: "#ffffff",
+      date: "May 2020 - Aug 2021",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Automated manual ranking process through the use of Excel VBAs",
+        "Optimised hiring procedures through the use of Python, VBA and RPA bots",
+        "Played a key role in facilitating the change management process for the introduction of an innovative performance management system, impacting over 600 employees positively.",
       ],
     },
     {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
+      title: "Digital Transformation Program Analyst",
+      company_name: "Hewlett Packard Enterprise",
+      icon: hpe,
       iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
+      date: "Aug 2021 - Apr 2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Part of the digital team leading a project to integrate HPE’s global operations internal and external data into one platform with Hyperledger tracking",
+        "Responsible for creating Business Requirement Documents"
       ],
     },
     {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
+      title: "Fullstack Blockchain Engineer",
+      company_name: "Nex10 Labs",
+      icon: nex10,
+      iconBg: "#ffffff",
+      date: "Mar 2022 - Jun 2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Fullstack developer for Uninterested Unicorns collection",
+        "Fullstack developer for NFT Marketplace and 3D Voxel collection for Uninterested Unicorns",
+        "Collaborated with 3D artists and blockchain experts to incorporate innovative features, such as unique NFT attributes and decentralized storage solutions",
       ],
     },
     {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
+      title: "Application Developer",
+      company_name: "IBM",
+      icon: ibm,
       iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
+      date: "Jul 2022 - Present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Frontend Developer for MINDEF mobile application",
+        "Led the project through multiple stages, from initial pilot to subsequent full-scale delivery releases. Both the mobile app and the admin web portal are now successfully deployed nationwide.",
+        "Applied Scrum and Agile methodologies to efficiently manage and deliver project",
+        "Post production support through upgrading AWS AMI and bug fixing",
       ],
     },
   ];
+
+  const techItems = [
+    {tech_name: 'React', tech_color: "#00D1F7"},
+    {tech_name: 'React Native', tech_color: "#FFFFFF"},
+    {tech_name: 'Spring Boot', tech_color: "#ff00f6"},
+    {tech_name: 'MySQL', tech_color: "#f35d40"},
+    {tech_name: 'AWS', tech_color: "#0AE448"},
+  ]
   
   const testimonials = [
     {
@@ -262,4 +266,4 @@ import {
     },
   ];
   
-  export { services, technologies, experiences, testimonials, projects };
+  export { services, technologies, experiences, techItems, testimonials, projects };
