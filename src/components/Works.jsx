5337068105d467ae8a8353288b87be073c86764b
norcodes987 <norcodes987@gmail.com>
2024-01-19 00:01:13 +0800

    works

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,149 @@
-import React from 'react'
+import React, {useRef, useState} from "react";
+import {Tilt}  from 'react-tilt'
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+      >
+        <div className='relative w-full h-[230px]'>
+          <img
+            src={image}
+            alt='project_image'
+            className='w-full h-full object-cover rounded-2xl'
+          />
+
+          <div className='absolute inset-0 flex justify-end m-3 card-img_hover'>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img
+                src={github}
+                alt='source code'
+                className='w-1/2 h-1/2 object-contain'
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-5'>
+          <h3 className='text-white font-bold text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[14px]'>{description}</p>
+        </div>
+
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+  );
+};
 
 const Works = () => {
-  return (
-    <div>Works</div>
-  )
-}
+  const [data,setData] = useState(projects);
+  const [selectedButton, setSelectedButton] = useState('All');
 
-export default Works
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+  });
+
+  const gallery_filter = (type) => {
+    setSelectedButton(type);
+    if (type === 'All') {
+      setData(projects);
+    } else {
+      const filteredProjects = projects.filter((project) => project.type === type);
+      setData(filteredProjects);
+    }
+  };
+
+
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} `}>My work</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+      </motion.div>
+
+      <div className='w-full flex' ref={ref}>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          The following projects showcases my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories. It reflects my ability to solve complex problems, 
+          work with different technologies,
+          and manage projects effectively.
+        </motion.p>
+      </div>
+     
+      <div className="mt-20">
+          <ul className="flex items-center justify-center list-none gap-4 mb-30">
+            <li>
+              <button 
+                onClick={() => gallery_filter('All')}
+                className={`${
+                  selectedButton === 'All' ? 'bg-blue-500' : 'bg-none'
+                } text-white py-2 px-5 uppercase border-navajowhite text-lg rounded-full`}
+              >All
+              </button>
+            </li>
+            {[...new Set(projects.map((project) => project.type))].map((item) => (
+              <li key={item}>
+                <button
+                  onClick={() => gallery_filter(item)}
+                  className={`${
+                    selectedButton === item ? 'bg-blue-500' : 'bg-none'
+                  } text-white py-2 px-5 uppercase border-navajowhite text-lg rounded-full`}
+                >
+                  {item}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <motion.div style={{ scaleX}} className="h-2 bg-white mt-5"></motion.div>
+      <div className='mt-10 flex flex-wrap gap-7 sm:items-center'>
+        {data.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "");
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,18 +3,6 @@ import {
     backend,
     creator,
     web,
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
-    redux,
-    tailwind,
-    nodejs,
-    mongodb,
-    git,
-    figma,
-    docker,
     ibm,
     mci,
     hpe,
@@ -22,7 +10,6 @@ import {
     carrent,
     jobit,
     tripguide,
-    threejs,
   } from "../assets";
   
   export const navLinks = [
@@ -59,60 +46,6 @@ import {
     },
   ];
   
-  const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
-    {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "Three JS",
-      icon: threejs,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "figma",
-      icon: figma,
-    },
-    {
-      name: "docker",
-      icon: docker,
-    },
-  ];
   
   const experiences = [
     {
@@ -203,6 +136,7 @@ import {
   const projects = [
     {
       name: "Car Rent",
+      type: 'Full Stack',
       description:
         "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
       tags: [
@@ -224,6 +158,7 @@ import {
     },
     {
       name: "Job IT",
+      type: 'Dev Ops',
       description:
         "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
       tags: [
@@ -245,6 +180,7 @@ import {
     },
     {
       name: "Trip Guide",
+      type: 'Scripting',
       description:
         "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
       tags: [
@@ -266,4 +202,4 @@ import {
     },
   ];
   
-  export { services, technologies, experiences, techItems, testimonials, projects };
+  export { services, experiences, techItems, testimonials, projects };
